dcb7ab0f9b8d4f561ac54014fb712a7426e14bec
liziniu <374387855@qq.com>
2019-11-29 20:44:33 +0800

    update collect samples

--- collect_sample.py
+++ collect_sample.py
@@ -1,9 +1,9 @@
-import gym
 import numpy as np
 import argparse
 import datetime
 import os
 import os.path as osp
+from scipy import stats
 import pickle
 
 from stable_baselines.sac.policies import MlpPolicy
@@ -12,6 +12,77 @@ from stable_baselines import logger
 from stable_baselines.common.cmd_util import make_mujoco_env
 
 from train_online import MujocoSaver
+
+
+class NoiseModel(object):
+    def __init__(self, ac_space, model, noise_type, seed=2019):
+        self.ac_space = ac_space
+        self.ac_space.seed(seed)
+        self.model = model
+        self.noise_type = str(noise_type)
+
+        self.multi_variate_norm_pd = stats.multivariate_normal
+
+    def step(self, obs):
+        if 'random' in self.noise_type:
+            acs = self.ac_space.sample()
+            logps = self.multi_variate_norm_pd.logpdf(acs, mean=np.zeros_like(acs))
+        else:
+            acs, logps = self.model.policy_tf.step(obs[None])
+            acs, logps = acs.flatten(), logps.flatten()
+            if 'eps' in self.noise_type:
+                eps = float(self.noise_type.split('_')[-1])
+                randomized = np.random.uniform() < eps
+                if randomized:
+                    acs = self.ac_space.sample()
+                    logps = self.model.policy_tf.evaluate_logp(obs[None], acs[None]).flatten()
+            elif 'normal' in self.noise_type:
+                std = float(self.noise_type.split('_')[-1])
+                normal_noise = np.random.normal(loc=0, scale=std, size=np.shape(acs))
+                acs += normal_noise
+                logps = self.multi_variate_norm_pd.logpdf(normal_noise, mean=np.zeros_like(normal_noise))
+            elif self.noise_type == 'none':
+                pass
+            else:
+                raise ValueError('{} is not recognized'.format(self.noise_type))
+        assert acs.ndim == 1  and logps.ndim == 1
+        return acs, logps
+
+
+def collect_trajectories(env, model, num_transitions, savedir):
+    nb_samples = 0
+
+    obs = env.reset()
+    trajs, rets, lens = [], [], []
+    mb_obs, mb_acs, mb_rs, mb_dones, mb_mus = [], [], [], [], []
+    while nb_samples < num_transitions:
+        acs, logps = model.step(obs)
+        mb_obs.append(obs)
+        mb_acs.append(acs)
+        mb_mus.append(logps)
+        obs, rewards, dones, infos = env.step(acs)
+        mb_rs.append(rewards)
+        mb_dones.append(dones)
+        if dones:
+            traj = dict(obs=np.asarray(mb_obs, dtype=env.observation_space.dtype),
+                        acs=np.asarray(mb_acs, dtype=env.action_space.dtype),
+                        rs=np.asarray(mb_rs, dtype=np.float32),
+                        dones=np.asarray(mb_dones, dtype=np.bool),
+                        mus=np.exp(np.asarray(mb_mus, dtype=np.float32)))
+            trajs.append(traj)
+            rets.append(np.sum(mb_rs))
+            lens.append(len(mb_obs))
+            nb_samples += len(mb_obs)
+            with open(osp.join(savedir, 'samples_0.pkl'), 'ab+') as f:
+                pickle.dump(traj, f)
+            mb_obs, mb_acs, mb_rs, mb_dones, mb_mus = [], [], [], [], []
+            obs = env.reset()
+    logger.info("*******************************")
+    logger.info("Total Trajectory: {}".format(len(trajs)))
+    logger.info("Average Return: {}".format(np.mean(rets)))
+    logger.info("Average Length: {}".format(np.mean(lens)))
+    logger.info("*******************************")
+    return trajs
 
 
 def main(args):
@@ -31,29 +102,18 @@ def main(args):
     logger.info("*********Trying load model params from: {}*********".format(load_path))
     model.load_parameters(load_path)
 
-    model.learn(total_timesteps=int(args.total_timesteps), log_interval=10, save_samples=True,
-                model_save_interval=args.save_interval)
-    modelsave_dir = osp.join(logger.get_dir(), "checkpoints")
-    os.makedirs(modelsave_dir, exist_ok=True)
-    modelsave_path = osp.join(modelsave_dir, "final")
-    model.save(modelsave_path)
-    logger.info("save SAC model into: {}".format(modelsave_path))
+    model = NoiseModel(ac_space=env.action_space, model=model,
+                       noise_type=args.noise_type, seed=args.seed)
 
-    # del model  # remove to demonstrate saving and loading
-    #
-    # model = SAC.load("sac_pendulum")
-    #
-    # obs = env.reset()
-    # while True:
-    #     action, _states = model.predict(obs)
-    #     obs, rewards, dones, info = env.step(action)
-    #     env.render()
+    savedir = osp.join(logger.get_dir(), "runner")
+    os.makedirs(savedir)
+    collect_trajectories(env, model, num_transitions=args.total_timesteps, savedir=savedir)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', type=str, default='Hopper-v2')
     parser.add_argument('--seed', type=int, default=2019)
     parser.add_argument('--load_dir', type=str, default='dataset')
-    parser.add_argument('--total_timesteps', type=float, default=1e3)
-    parser.add_argument('--save_interval', type=float, default=5e5)
+    parser.add_argument('--total_timesteps', type=float, default=1e4)
+    parser.add_argument('--noise_type', type=str, default='none')
     main(parser.parse_args())
